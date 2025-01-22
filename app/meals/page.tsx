e66b6a41065e5f8b8ed183cbf27c82934cb11022
Alanto Manu <alantomanu501@gmail.com>
2025-01-22 23:06:12 +0530

    Revamp MealsPage layout with a new header, engaging content, and navigation link to share recipes. Introduced CSS modules for styling and integrated MealsGrid component for meal display.

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>
-      <h1>meals main</h1>
-    </div>
+import Link from "next/link"
+import classes from "./page.module.css"
+export default function MealsPage(){
+  return(
+    <>
+    <header className={classes.header}>
+      <h1>
+        Tasty meals, created <span className={classes.highlight}>by you</span>
+      </h1>
+      <p>
+        Choose your favourite recipe & cook yourself, Enjoy!! 
+      </p>
+      <p>
+        <Link href='/meals/share'>
+        Share your Favourite Recipe
+        </Link>
+      </p>
+      <main className={classes.main}>
+         <MealsGrid meals={[]}></MealsGrid>
+      </main>
+    </header>
+    </>
   )
 }
-
-export default page
